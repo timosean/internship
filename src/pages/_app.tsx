596d9769871b3336e88a1a7b405bf8ace315f41c
Seungwoo Lee <timosean@naver.com>
2023-02-07 23:13:27 +0900

    Feat: 전체 레이아웃에 recoil 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,17 @@ import Header from "@/components/Header";
 import "@/styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <Header />
       <main className={`${inter.className} px-[16px]`}>
         <Component {...pageProps} />
       </main>
-    </>
+    </RecoilRoot>
   );
 }
