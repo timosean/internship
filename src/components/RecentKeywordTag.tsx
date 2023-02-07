ad2501d7e51a54b1b40e48aa61247eaecb0b23e8
Seungwoo Lee <timosean@naver.com>
2023-02-07 22:09:43 +0900

    Feat: 태그가 자연스럽게 배치되도록 float-left 적용

--- src/components/RecentKeywordTag.tsx
+++ src/components/RecentKeywordTag.tsx
@@ -2,7 +2,7 @@ import { GrClose } from "react-icons/gr";
 
 const RecentKeywordTag = ({ content }: { content: string }) => {
   return (
-    <div className="w-fit px-[8px] py-[5px] flex items-center justify-center gap-x-[10px] rounded-[2px] border-[0.6px] border-NAILGRAY">
+    <div className="float-left w-fit px-[8px] py-[5px] mr-[13px] mb-[8px] flex items-center justify-center gap-x-[10px] rounded-[2px] border-[0.6px] border-NAILGRAY">
       <span className="text-NAILGRAY text-[11px] leading-[13px] tracking-[-0.02em]">
         {content}
       </span>
