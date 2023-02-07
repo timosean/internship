43bb63bbcb29e0532b30c1a9a63c4e9d70fe35ed
Seungwoo Lee <timosean@naver.com>
2023-02-07 22:09:58 +0900

    Feat: 검색 페이지 완성

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,50 @@
 import type { NextPage } from "next";
+import RecentKeywordTag from "@/components/RecentKeywordTag";
+import { useEffect, useState } from "react";
+
+const RecentKeywordTagContainer = ({ keywords }: { keywords: string[] }) => {
+  return (
+    <div className="mt-[10px] w-full">
+      {keywords.map((keyword) => (
+        <RecentKeywordTag content={keyword} />
+      ))}
+    </div>
+  );
+};
+
+const NoKeywordNotice = () => {
+  return (
+    <div className="mt-[10px] w-full h-[110px] flex justify-center align-center">
+      <p>최근 검색한 키워드가 없습니다!</p>
+    </div>
+  );
+};
 
 const SearchPage: NextPage = () => {
-  return <div>하하하하하 서치페이지</div>;
+  const [keywords, setKeywords] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const recentKeywords = localStorage.getItem("recent-keywords");
+      if (recentKeywords) {
+        const JSONKeywords = JSON.parse(recentKeywords);
+        for (const key in JSONKeywords) {
+          setKeywords((prev) => prev.concat(key));
+        }
+      }
+    }
+  }, []);
+
+  return (
+    <div className="mt-6">
+      <h1 className="font-bold text-base">최근 검색어</h1>
+      {keywords ? (
+        <RecentKeywordTagContainer keywords={keywords} />
+      ) : (
+        <NoKeywordNotice />
+      )}
+    </div>
+  );
 };
 
 export default SearchPage;
