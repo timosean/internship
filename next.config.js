d5d982a02bec8ef8e32be8b4eb5e0f4fc7c38693
Seungwoo Lee <timosean@naver.com>
2023-02-08 13:02:26 +0900

    Fix: 외부 이미지 불러와지지 않는 에러 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["d1rdczpw822fy3.cloudfront.net"],
+  },
 };
 
 module.exports = nextConfig;
