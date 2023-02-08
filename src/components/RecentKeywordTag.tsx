028fd84bdfe3af348a64ca1814f6f2e8f5f299f5
Seungwoo Lee <timosean@naver.com>
2023-02-08 16:58:27 +0900

    Feat: 최근 검색어 태그를 클릭해도 검색결과 페이지로 이동

--- src/components/RecentKeywordTag.tsx
+++ src/components/RecentKeywordTag.tsx
@@ -1,6 +1,7 @@
 import { useSetRecoilState } from "recoil";
 import { recentKeywordState } from "@/recoil/atoms/recentKeywordState";
 import { GrClose } from "react-icons/gr";
+import Link from "next/link";
 
 const RecentKeywordTag = ({ content }: { content: string }) => {
   const setRecentKeyword = useSetRecoilState(recentKeywordState);
@@ -27,7 +28,7 @@ const RecentKeywordTag = ({ content }: { content: string }) => {
   return (
     <div className="float-left w-fit px-[8px] py-[5px] mr-[13px] mb-[8px] flex items-center justify-center gap-x-[10px] rounded-[2px] border-[0.6px] border-NAILGRAY">
       <span className="text-NAILGRAY text-[11px] leading-[13px] tracking-[-0.02em]">
-        {content}
+        <Link href={`/searchResult/${content}`}>{content}</Link>
       </span>
       <GrClose size={10.16} id="tag-close" onClick={() => RemoveTag(content)} />
     </div>
