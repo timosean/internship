1910a2cb541ebcee373346f00bf020648cdf0d90
Seungwoo Lee <timosean@naver.com>
2023-02-08 12:17:29 +0900

    Feat: 네일샵 검색결과 받아오기 완료

--- src/pages/searchResult/[keyword].tsx
+++ src/pages/searchResult/[keyword].tsx
@@ -1,10 +1,44 @@
 import { useRouter } from "next/router";
+import axiosInstance from "@/utils/axiosInstance";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { searchingState } from "@/recoil/atoms/searchingState";
+
+interface ShopInfo {
+  shop_id: string;
+  location: string;
+  name: string;
+}
+
+interface NailShop {
+  shop: ShopInfo;
+  is_new: boolean;
+  categories: number[];
+  nail_id: string;
+  thumb_images: string;
+}
 
 const SearchResult = () => {
   const router = useRouter();
   const { keyword } = router.query;
 
-  return <p>검색어: {keyword}</p>;
+  const [_, setIsSearching] = useRecoilState(searchingState);
+  const [results, setResult] = useState<NailShop[]>([]);
+
+  useEffect(() => {
+    setIsSearching(true);
+  }, []);
+
+  useEffect(() => {
+    const getNailShopResult = async () => {
+      const res = await axiosInstance.get(`nails?input=${keyword}`);
+      setResult(res.data.data.nails);
+    };
+
+    getNailShopResult();
+  }, []);
+
+  return <div className="w-full">{JSON.stringify(results)}</div>;
 };
 
 export default SearchResult;
