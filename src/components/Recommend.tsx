c6ddaf9491ffedbee6a7bea67890e8b557700a44
Seungwoo Lee <timosean@naver.com>
2023-02-09 09:49:26 +0900

    Design: 추천 검색어 폰트 굵게

--- src/components/Recommend.tsx
+++ src/components/Recommend.tsx
@@ -7,6 +7,35 @@ interface RecommendItem {
   imgsrc: string | null;
 }
 
+const MarkedText = ({ text: string }) => {
+  return <span className="text-NAILPINK font-bold">text</span>;
+};
+
+const matchedText = (text: string, keyword: string) => {
+  //키워드가 빈 값이 아니거나, text가 키워드를 포함하고 있다면(조건)
+  if (keyword !== "" && text.includes(keyword)) {
+    //키워드를 기준으로 text를 쪼갠다.
+    const parts = text.split(new RegExp(`(${keyword})`, "gi"));
+
+    return (
+      <>
+        // 문자열이 담긴 배열을 map 돌림
+        {parts.map((part, index) =>
+          //소문자로 변환 후 비교하여 일치하면 MarkedText 스타일 적용
+          part.toLowerCase() === keyword.toLowerCase() ? (
+            <MarkedText key={index} text={part} />
+          ) : (
+            //일치하지않으면 그대로 출력
+            part
+          )
+        )}
+      </>
+    );
+  }
+
+  return text;
+};
+
 const removeHashtag = (word: string) => {
   if (word.includes("#")) return word.slice(1);
   else return word;
@@ -31,7 +60,7 @@ const Recommend = ({ type, name, imgsrc }: RecommendItem) => {
         ) : (
           <FaHashtag size={20} className="text-NAILGRAY" />
         )}
-        <span className="ml-[10px]">{removeHashtag(name)}</span>
+        <span className="ml-[10px] font-bold">{removeHashtag(name)}</span>
       </div>
       <div className="text-sm text-NAILGRAY flex items-center">
         {type === "shops" ? "네일샵" : "해시태그"}
